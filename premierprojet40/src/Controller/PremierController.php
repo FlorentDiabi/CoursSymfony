2932775ecb1869de8403a0d5e2128f5a5b1b1fd1
Clément <clementloze@hotmail.fr>
2021-11-30 12:02:31 +0100

    avancement tp

---
+++ premierprojet40/src/Controller/PremierController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PremierController {
+    /**
+     * @Route("/index", name="index")
+     */
+    public function index() {
+        return new Response($coontent= 'Bonjour mon premier contrôleur');
+    }
+}
---
+++ premierprojet40/src/Controller/PrincipalController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PrincipalController extends AbstractController
+{
+    /**
+     * @Route("/principal", name="principal")
+     */
+    public function index(): Response
+    {
+        return $this->render('principal/index.html.twig', [
+            'controller_name' => 'Symfony c\'est super',
+        ]);
+    }
+    
+    /**
+     * @Route("/welcome/{nom}", name="welcome")
+     */
+    public function welcome(string $nom){
+        return $this->render('principal/welcome.html.twig', array(
+            "nom" => $nom
+        ));
+    }
+    
+    /**
+     * @Route("/message/{departement}&{sexe}", name="message")
+     */
+    public function message(string $departement, string $sexe){
+        if($sexe == "garçon"){
+            $sexe = "un garçon ";
+        } 
+        elseif ($sexe == "fille") 
+        {
+            $sexe = "une fille ";
+        }
+        else {
+            $sexe = "de sexe inconnu !";
+        }
+        
+        return $this->render('principal/message.html.twig', array(
+            "departement" => $departement,
+            "sexe" => $sexe,
+        ));
+    }
+}
